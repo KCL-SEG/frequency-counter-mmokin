e57b1bfd4461567a1e6153d3d7e18f1ee6023cc8
Michael (Michael) Alelegn <michael.alelegn@kcl.ac.uk>
2023-12-10 17:55:59 +0000

    attempt 1

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        key = str(item)
+        if key not in frequencies:
+            frequencies[key] = 0
+        frequencies[key] += 1
     # Your code goes here
     return frequencies
